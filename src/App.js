17d9bff372e25ad7b3712f8c5f32e903390750fb
Ilya Soloveychik <dj.active.17@gmail.com>
2020-04-07 17:22:50 +0300

    add mediaqueries

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import Header from "./components/Header";
 import Sidebar from "./components/Sidebar";
 import Main from "./components/Main";
 
+import './index.css';
+import './mediaqueries.css';
+
 function App() {
   return (
         <div className="main-content">
