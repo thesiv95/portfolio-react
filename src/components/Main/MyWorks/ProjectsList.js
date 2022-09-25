a7110087c5856ef3e7eb8a59318692a7c474bf5d
Ilya Soloveychik <dj.active.17@gmail.com>
2022-09-25 18:42:04 +0300

    Revert "replace to real projects"
    This reverts commit bcc28bdee973370e72ff63601fcb0b8fd278df82.

--- src/components/Main/MyWorks/ProjectsList.js
+++ src/components/Main/MyWorks/ProjectsList.js
@@ -9,12 +9,12 @@ export default class ProjectsList extends React.Component {
                     this.props.data.map((item, index) =>
                         <li key={index} className="projects-item">
                             <div className="hover-img">
-                                <img className="project-img" src={'projects/' + item.previewPicFile} alt={item.siteDomain}/>
+                                <img className="project-img" src={item.previewPicUrl} alt={item.siteDomain}/>
                                 <div className="zoom-wrapper">
-                                    <a href={item.siteDomain} className="zoom-link" rel="noopener noreferrer" target="_blank">{item.siteName}</a>
+                                    <a href={`${item.https ? 'https' : 'http'}://${item.siteDomain}`} className="zoom-link" rel="noopener noreferrer" target="_blank">{item.siteName}</a>
                                 </div>
                             </div>
-                            <a href={item.siteDomain} className="project-link" rel="noopener noreferrer" target="_blank">{item.siteName}</a>
+                            <a href={`${item.https ? 'https' : 'http'}://${item.siteDomain}`} className="project-link" rel="noopener noreferrer" target="_blank">{item.siteName}</a>
                             <div className="project-desc">
                                 {item.description}
                             </div>
