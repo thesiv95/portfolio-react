5ac44de76c8963056ce250dfef9df6e7b6a6d5e6
Ilya Soloveychik <dj.active.17@gmail.com>
2024-05-01 18:55:34 +0300

    add webservers stack

--- src/data/skills.js
+++ src/data/skills.js
@@ -1,6 +1,6 @@
 export default [
     'Node.js/NPM', 'Express.js', 'Nest.js', 'Swagger.js', 'Yarn', 'MySQL/PostgreSQL', 'MongoDB', 'Redis', 'React.js', 
-    'Redux', 'Bootstrap', 'webpack', 'Python', 'AWS', 'Git', 'Github', 'Bitbucket', 'YouTrack', 'Docker', 'Kubernetes', 'CircleCI', 
+    'Redux', 'Bootstrap', 'webpack', 'Python', 'AWS', 'Git', 'Github', 'Bitbucket', 'YouTrack', 'nginx', 'LAMP stack', 'Docker', 'Kubernetes', 'CircleCI', 
     'Github Actions', 'Figma', 'Visual Studio Code', 'IntellijIDEA WebStorm', 'Sublime Text 2', 'Ubuntu', 'Debian', 'AWS EC2'
 ];
 
