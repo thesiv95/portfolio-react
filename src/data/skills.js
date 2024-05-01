e6a65d6119d9b22648f99c4ed69effcce488f543
Ilya Soloveychik <dj.active.17@gmail.com>
2024-05-01 18:47:05 +0300

    update data

--- src/data/skills.js
+++ src/data/skills.js
@@ -1,16 +1,6 @@
 export default [
-    'TypeScript',
-    'JavaScript',
-    'HTML5',
-    'CSS3',
-    'Node.js',
-    'Heroku',
-    'AWS',
-    'Git',
-    'VSCode',
-    'MySQL',
-    'MongoDB',
-    'Windows',
-    'Ubuntu',
+    'Node.js/NPM', 'Express.js', 'Nest.js', 'Swagger.js', 'Yarn', 'MySQL/PostgreSQL', 'MongoDB', 'Redis', 'React.js', 
+    'Redux', 'Bootstrap', 'webpack', 'Python', 'AWS', 'Git', 'Github', 'Bitbucket', 'YouTrack', 'Docker', 'Kubernetes', 'CircleCI', 
+    'Github Actions', 'Figma', 'Visual Studio Code', 'IntellijIDEA WebStorm', 'Sublime Text 2', 'Ubuntu', 'Debian', 'AWS EC2'
 ];
 
